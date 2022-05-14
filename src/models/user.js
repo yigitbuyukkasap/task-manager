ece55c97655a8d68e1ac6e706790379087a74d04
yigitbuyukkasap <yigitbuyukkasap@gmail.com>
2022-05-14 16:36:05 +0300

    initial commit

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email dogru degil')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Sifre password icermemeli')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Yas pozitif bir sayi olmali')
+            }
+        }
+    }
+})
+
+module.exports = User;
