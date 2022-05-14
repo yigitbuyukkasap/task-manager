a01dc253914c60b4b0ec69fc655426b4f99f0234
yigitbuyukkasap <yigitbuyukkasap@gmail.com>
2022-05-15 01:23:26 +0300

    adding patch users and tasks

--- src/index.js
+++ src/index.js
@@ -50,6 +50,52 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(404).send({error: 'Invalid update!'})
+    }
+
+    const _id = req.params.id;
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            res.status(404).send()
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(404).send({error: 'Invalid update!'})
+    }
+
+    const _id = req.params.id;
+    try {
+        const user = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            res.status(404).send()
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
     try {
