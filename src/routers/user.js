587d0fac41400a2ae515c7514b804c6c86b6d659
yigitbuyukkasap <yigitbuyukkasap@gmail.com>
2022-05-15 16:09:08 +0300

    adding logout, logoutAll and no longer exposing tokens and password to api response

--- src/routers/user.js
+++ src/routers/user.js
@@ -26,6 +26,28 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
 router.get('/users/me', auth, async (req, res) => {
     try {
         const users = await User.find()
@@ -34,6 +56,8 @@ router.get('/users/me', auth, async (req, res) => {
         res.status(500).send()
     }
 })
+
+
 
 router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
