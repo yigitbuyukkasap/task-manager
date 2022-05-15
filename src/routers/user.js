8042008c70f5fbc21a6ae3fc7992dc81f8d16ed6
yigitbuyukkasap <yigitbuyukkasap@gmail.com>
2022-05-15 19:14:18 +0300

    fetching avatar data and displaying endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -102,4 +102,26 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     res.status(400).send({ error: error.message })
 })
 
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save()
+
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'images/jpg')
+        res.send(user.avatar)
+
+    } catch (error) {
+        res.status(404).send();
+    }
+})
 module.exports = router;
