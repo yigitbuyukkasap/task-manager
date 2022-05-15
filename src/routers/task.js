914e463aa2cd8ccb535dbf57d83512a284dc1882
yigitbuyukkasap <yigitbuyukkasap@gmail.com>
2022-05-15 18:35:54 +0300

    adding desc asc by createdAt - sort

--- src/routers/task.js
+++ src/routers/task.js
@@ -60,13 +60,23 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
 
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+
+    }
     try {
         //const tasks = await Task.find({ owner: req.user._id })
         await req.user.populate({
@@ -74,7 +84,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate();
         res.send(req.user.tasks);
