048fc24f24bcb9404d8d27a75ded1f876a46a92f
yigitbuyukkasap <yigitbuyukkasap@gmail.com>
2022-05-15 19:00:11 +0300

    adding multer and image filtering

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const router = new express.Router()
 const auth = require('../middleware/auth');
+const multer = require('multer');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -77,6 +78,26 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (error) {
         res.status(500).send(error);
     }
+})
+
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match('/\.(png|jpg|jpeg)$/')) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), async (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router;
